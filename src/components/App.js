20f090d6e6057ace77cbcdfa9f4db9de9bf9b0c7
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-01-22 04:24:19 -0600

    Initial project setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Editor from "./Editor"
+import Preview from "./Preview";
+
+function App() {
+  return (
+    <>
+      <Editor />
+      <Preview />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Editor.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Editor = () => {
+    return (
+        <>
+            <textarea id="editor"></textarea>
+        </>
+    );
+};
+
+export default Editor;
