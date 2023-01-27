7a654e726b4080ee54bd1ebac7d7c38540d20368
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-01-27 11:49:46 -0600

    Used Marked library to parse markdown to HTML

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { marked } from "marked";
 
 const Preview = (props) => {
     return (
         <>
             <div id="preview-box">
                 <p>Preview</p>
-                <div id="preview">{props.markdown}</div>
+                <div id="preview" dangerouslySetInnerHTML={{__html: marked.parse(props.markdown)}}></div>
             </div>
         </>
     );
