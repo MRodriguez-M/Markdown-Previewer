580793de533490e61ab801476a1e024308800089
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-01-27 17:42:31 -0600

    Added markdown demo as initial state

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,26 @@ import Preview from "./Preview";
 import { useState } from "react";
 
 function App() {
-  const [markdown, setMarkdown] = useState("");
+  const [markdown, setMarkdown] = useState(
+    "# Welcome to My Markdown Previewer!\n" + "---\n"
+    + "## Here are some of the things you can do...\n"
+    + "- You can create [links](https://www.markdownguide.org/basic-syntax/)\n"
+    + "- Write inline code (`console.log('This is inline code!')`) as part of a sentence\n"
+    + "- Write code blocks:\n"
+    + "```\n"
+    + "    if(sampleVar == 25) {\n"
+    + "      return console.log('Print this line!'); }\n"
+    + "```\n"
+    + "- Create lists:\n"
+    + "    1. Item one\n"
+    + "    1. Item two\n"
+    + "    1. Item three\n"
+    + "> Write text in blockquotes. Check out the spacing that blockquotes creates! Pretty neat.\n"
+    + "- Add an image:\n"
+    + "![Jolteon](/assets/jolteon.png)\n\n"
+    + "This is the pokemon **Jolteon**!\n\n"
+    + "There are many more things you can do with Markdown syntax and you can check them out at the link provided in the first bullet point. Enjoy practicing Markdown!"
+  );
 
   const handleChange = (e) => {
     setMarkdown(e.target.value);
