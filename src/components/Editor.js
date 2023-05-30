23c6fffb47a7b324378561616022b6a00f88bffe
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-05-30 17:47:17 -0600

    Styled clear button

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,8 +4,10 @@ const Editor = (props) => {
     return (
         <>
             <div id="editor-box">
-                <h3>Editor</h3>
-                <button onClick={props.handleClick}>Clear</button>
+                <div id="editor-header">
+                    <h3>Editor</h3>
+                    <button id="clear-button" onClick={props.handleClick}>Clear</button>
+                </div>
                 <textarea id="editor" value={props.markdown} onChange={props.handleChange}></textarea>
             </div>
         </>
